f250d4351550aefc47503d8f86bd551637b6a878
hayatniazi <66178207+hayatniazi@users.noreply.github.com>
2020-06-15 23:48:39 +0100

    updating

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -5,12 +5,7 @@ import AppReducer from './AppReducer';
 
 const initialState = {
 
-    transaction: [
-          { id: 1, text: 'Flower', amount: -20 },
-          { id: 2, text: 'Salary', amount: 300 },
-          { id: 3, text: 'Book', amount: -10 },
-          { id: 4, text: 'Camera', amount: 150 }
-         ]
+    transaction: []
 }
 
 // create global context using createContext of React library
